4e5ad9be6b75ff0ed9dfa64ddb042965097c302c
Mario Dian <mariodian@gmail.com>
2024-06-03 10:51:42 +0800

    don't reset existing system message

--- src/components/Chat/ChatContent/ClearChat.tsx
+++ src/components/Chat/ChatContent/ClearChat.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import useStore from '@store/store';
 
-import { ChatInterface } from '@type/chat';
+import { ChatInterface, MessageInterface } from '@type/chat';
 import { _defaultSystemMessage } from '@constants/chat';
 
 const ClearChat = React.memo(() => {
@@ -12,10 +12,15 @@ const ClearChat = React.memo(() => {
   const currentChatIndex = useStore((state) => state.currentChatIndex);
 
   const clearChat = () => {
-    const updatedChats: ChatInterface[] = JSON.parse(
+    const chats = JSON.parse(
       JSON.stringify(useStore.getState().chats)
     );
-    updatedChats[currentChatIndex].messages = [{ role: 'system', content: useStore.getState().defaultSystemMessage }];
+    const updatedChats: ChatInterface[] = chats;
+    const systemMessage = updatedChats[currentChatIndex].messages
+      .filter((message: MessageInterface) => message.role === 'system')
+      .map((message: MessageInterface) => message.content).shift()
+      || useStore.getState().defaultSystemMessage;
+    updatedChats[currentChatIndex].messages = [{ role: 'system', content: systemMessage }];
     setChats(updatedChats);
   };
 
